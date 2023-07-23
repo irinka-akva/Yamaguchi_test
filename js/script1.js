b8ae81960453d8b79ec5140effed61c8572a5d87
Irina <irinka-akva@ya.ru>
2023-07-23 16:12:30 +0300

    Доделывает анимацию на второй странице

--- js/script1.js
+++ js/script1.js
@@ -2,16 +2,20 @@ const buttonUp = document.querySelector(".btn--up");
 const buttonDown = document.querySelector(".btn--down");
 const imgUp = document.querySelector(".ergonomics__img-top");
 
-buttonUp.addEventListener("click", () => {
-    if (!imgUp.classList.contains("up-js")) {
-        imgUp.classList.add("up-js");
-        imgUp.classList.remove("down-js");
-    }
-})
+if (buttonUp !== null) {
+    buttonUp.addEventListener("click", () => {
+        if (!imgUp.classList.contains("up-js")) {
+            imgUp.classList.add("up-js");
+            imgUp.classList.remove("down-js");
+        }
+    })
+}
 
-buttonDown.addEventListener("click", () => {
-    if (imgUp.classList.contains("up-js")) {
-        imgUp.classList.remove("up-js");
-        imgUp.classList.add("down-js");
-    }
-})
+if (buttonDown !== null) {
+    buttonDown.addEventListener("click", () => {
+        if (imgUp.classList.contains("up-js")) {
+            imgUp.classList.remove("up-js");
+            imgUp.classList.add("down-js");
+        }
+    })
+}
